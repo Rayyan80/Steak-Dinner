dcb62207b7928a8f1cb6cba448bf666c77776d54
ryan <60459219+Rayyan80@users.noreply.github.com>
2024-09-17 11:32:50 -0700

    Update SteakDinner.py

--- Unit 1 Project/SteakDinner.py
+++ Unit 1 Project/SteakDinner.py
@@ -2,19 +2,26 @@
 import turtle as trtl
 wn = trtl.Screen()
 pen = trtl.Turtle()
-
-wn.bgcolor("gray")
+wn.bgpic("Table.png")
 #--------------------------------------#
 
 #--------------INPUTS--------------#
+steakRareity = input("How do you want your steak cooked? [Rare, Meduim Rare, Well Done]")
+#--------------------------------------#
 
+#--------------STEAK VARABLES--------------#
+rare = "#e87968"
+medumRare = "#a14b3a"
+wellDone = "#612b20"
 #--------------------------------------#
 
 def plate(pen): # PLATE FUNCTION #
     pen.fillcolor("white")
     pen.color("white")
+    pen.width(1)
     pen.penup()
     pen.setposition(-400, -250)
+    pen.setheading(0)
     pen.pendown()
     pen.forward(800)
 
@@ -39,6 +46,46 @@ def plate(pen): # PLATE FUNCTION #
     pen.forward(700)
     pen.end_fill()
 
+def steak(pen, steakRareity):   # STEAK FUNCTION #
+    pen.penup()
+    pen.width(30)
+    pen.color("#543421")
+
+    if(steakRareity == "Rare"):
+        pen.fillcolor(rare)
+    elif(steakRareity == "Meduim Rare"):
+        pen.fillcolor(medumRare)
+    elif(steakRareity == "Well Done"):
+        pen.fillcolor(wellDone)
+        
+    pen.setposition(-370, -236)
+    pen.pendown()
+
+    pen.begin_fill()
+    pen.setheading(90)
+    pen.forward(120)
+    pen.right(90)
+    pen.forward(230)
+    pen.right(90)
+    pen.forward(120)
+    pen.right(90)
+    pen.forward(230)
+    pen.end_fill()
+
+    pen.penup()
+    pen.setposition(-400, -236)
+    pen.color("#d9c9ab")
+    pen.pendown()
+
+    pen.setheading(90)
+    pen.forward(120)
+    pen.left(90)
+    pen.forward(10)
+    pen.left(90)
+    pen.forward(120)
+
+
+steak(pen, steakRareity)
 plate(pen)
 #--------------DRAW TO SCREEN--------------#
 pen.hideturtle()
